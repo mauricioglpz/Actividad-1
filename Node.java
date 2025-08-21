fe4d6fd5504e42f24437e52983aac8d19bdbbda2
mauricioglpz <Al07023486@tecmilenio.mx>
2025-08-21 12:09:21 -0600

    Node.java

--- Node.java
+++ Node.java
@@ -1,14 +1,24 @@
 package actividad1;
 
 public class Node {
-    Object datos;
-    Node siguiente;
-    Node anterior; 
+    public Object datos;
+    public Node siguiente;
+    Node anterior;
+    public Node next;
+    public String data;
 
     public Node(Object datos) {
         this.datos = datos;
         this.siguiente = null;
         this.anterior = null;
     }
-}
 
+    public Object getDatos() { return datos; }
+    public void setDatos(Object datos) { this.datos = datos; }
+
+    public Node getSiguiente() { return siguiente; }
+    public void setSiguiente(Node siguiente) { this.siguiente = siguiente; }
+
+    public Node getAnterior() { return anterior; }
+    public void setAnterior(Node anterior) { this.anterior = anterior; }
+}
