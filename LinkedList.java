703b9d742a0bd19b4d2e1c41d4eab395bee41300
mauricioglpz <Al07023486@tecmilenio.mx>
2025-08-21 12:05:41 -0600

    LinkedList.java

--- LinkedList.java
+++ LinkedList.java
@@ -1,8 +1,8 @@
 package actividad1;
 
 public class LinkedList {
-    Node cabeza;
-    Node cola;
+    public Node cabeza;
+    public Node cola;
     String tipoLista; 
 
     public LinkedList(String tipoLista) {
@@ -25,7 +25,7 @@ public class LinkedList {
                 cola = nuevoNodo;
             } else if (tipoLista.equals("doble")) {
                 cola.siguiente = nuevoNodo;
-                nuevoNodo.anterior = cola;
+                nuevoNodo.setAnterior(cola);
                 cola = nuevoNodo;
             } else if (tipoLista.equals("circular")) {
                 nuevoNodo.siguiente = cabeza;
@@ -33,6 +33,90 @@ public class LinkedList {
                 cola = nuevoNodo;
             }
         }
+    }
+
+    // NUEVO MÉTODO: Insertar al inicio (eficiente para pilas)
+    public void insertarAlInicio(Object datos) {
+        Node nuevoNodo = new Node(datos);
+        if (cabeza == null) {
+            cabeza = nuevoNodo;
+            cola = nuevoNodo;
+            if (tipoLista.equals("circular")) {
+                cabeza.siguiente = cabeza;
+            }
+        } else {
+            nuevoNodo.siguiente = cabeza;
+            cabeza = nuevoNodo;
+            if (tipoLista.equals("doble")) {
+                cabeza.siguiente.anterior = cabeza;
+            } else if (tipoLista.equals("circular")) {
+                cola.siguiente = cabeza;
+            }
+        }
+    }
+
+    // NUEVO MÉTODO: Eliminar el primer elemento (eficiente para colas)
+    public Object eliminarPrimero() {
+        if (cabeza == null) {
+            return null;
+        }
+        Object datosEliminados = cabeza.datos;
+        
+        if (cabeza == cola) {
+            cabeza = null;
+            cola = null;
+        } else {
+            cabeza = cabeza.siguiente;
+            if (tipoLista.equals("doble") && cabeza != null) {
+                cabeza.anterior = null;
+            } else if (tipoLista.equals("circular")) {
+                cola.siguiente = cabeza;
+            }
+        }
+        return datosEliminados;
+    }
+
+    // NUEVO MÉTODO: Eliminar el último elemento (eficiente para pilas)
+    public Object eliminarUltimo() {
+        if (cabeza == null) {
+            return null;
+        }
+        Object datosEliminados = cola.datos;
+        
+        if (cabeza == cola) {
+            cabeza = null;
+            cola = null;
+        } else {
+            if (tipoLista.equals("doble")) {
+                cola = cola.anterior;
+                cola.siguiente = null;
+            } else if (tipoLista.equals("simple")) {
+                Node actual = cabeza;
+                while (actual.siguiente != cola) {
+                    actual = actual.siguiente;
+                }
+                cola = actual;
+                cola.siguiente = null;
+            } else if (tipoLista.equals("circular")) {
+                Node actual = cabeza;
+                while (actual.siguiente != cola) {
+                    actual = actual.siguiente;
+                }
+                cola = actual;
+                cola.siguiente = cabeza;
+            }
+        }
+        return datosEliminados;
+    }
+
+    // NUEVO MÉTODO: Obtener el primer elemento sin eliminarlo
+    public Object obtenerPrimero() {
+        return (cabeza != null) ? cabeza.datos : null;
+    }
+
+    // NUEVO MÉTODO: Obtener el último elemento sin eliminarlo
+    public Object obtenerUltimo() {
+        return (cola != null) ? cola.datos : null;
     }
 
     public boolean eliminar(Object datos) {
@@ -48,6 +132,8 @@ public class LinkedList {
                 cabeza = cabeza.siguiente;
                 if (tipoLista.equals("circular")) {
                     cola.siguiente = cabeza;
+                } else if (tipoLista.equals("doble") && cabeza != null) {
+                    cabeza.anterior = null;
                 }
             }
             return true;
@@ -71,6 +157,11 @@ public class LinkedList {
                 }
             }
             actual.siguiente = actual.siguiente.siguiente;
+            
+            if (tipoLista.equals("circular") && actual.siguiente == null) {
+                actual.siguiente = cabeza;
+            }
+            
             return true;
         }
 
@@ -83,7 +174,7 @@ public class LinkedList {
             return null;
         }
         do {
-            if (actual.datos.equals(datos)) {
+            if (actual.datos.equals(datos) || actual.datos == datos) {
                 return actual;
             }
             actual = actual.siguiente;
